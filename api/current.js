c40bda52b6fc031c9dd024dff7572e360494949f
deepkhara09-arch <deepkhara09@gmail.com>
2026-02-02 17:41:34 +0530

    Create current.js

---
+++ api/current.js
@@ -0,0 +1,25 @@
+export default function handler(req, res) {
+
+  const ORDER = ["workout","study","reading","test","mock"]
+  const q = req.query
+
+  let done = []
+
+  ORDER.forEach(t=>{
+    if(q[t]==="1") done.push(t)
+  })
+
+  let filename = "none_done.png"
+
+  if(done.length===1){
+    filename = `${done[0]}_done.png`
+  }
+  else if(done.length>=5){
+    filename = "all_done.png"
+  }
+  else if(done.length>1){
+    filename = `${done.join("_")}_done.png`
+  }
+
+  res.redirect(`/wallpapers/${filename}`)
+}
