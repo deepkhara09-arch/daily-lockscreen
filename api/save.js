29d982b3de572e7188478431548e555906ab7f62
deepkhara09-arch <deepkhara09@gmail.com>
2026-02-02 05:39:54 +0530

    Update save.js

--- api/save.js
+++ api/save.js
@@ -1,1 +1,8 @@
+import fs from "fs";
+import path from "path";
 
+export default function handler(req,res){
+  const file = path.join(process.cwd(),"state.json");
+  fs.writeFileSync(file, JSON.stringify(req.body,null,2));
+  res.status(200).json({ok:true});
+}
