75bcb5bb7d2a26f384514a680d8d1d57291fd91b
deepkhara09-arch <deepkhara09@gmail.com>
2026-02-02 15:36:05 +0530

    Update current.png.js

--- api/current.png.js
+++ api/current.png.js
@@ -1,3 +1,33 @@
+import fs from "fs";
+import path from "path";
+
 export default function handler(req, res) {
-  res.redirect("/wallpapers/workout_done.png");
+  try {
+    const statePath = path.join(process.cwd(), "state.json");
+    const state = JSON.parse(fs.readFileSync(statePath, "utf8"));
+
+    const done = [];
+
+    if (state.workout) done.push("workout");
+    if (state.study) done.push("study");
+    if (state.reading) done.push("reading");
+    if (state.test) done.push("test");
+    if (state.mock) done.push("mock_interview");
+
+    let file = "none_done.png";
+
+    if (done.length === 1) file = `${done[0]}_done.png`;
+    if (done.length > 1 && done.length < 5)
+      file = `${done.join("_")}_done.png`;
+    if (done.length === 5) file = "all_done.png";
+
+    const imagePath = path.join(process.cwd(), "wallpapers", file);
+    const image = fs.readFileSync(imagePath);
+
+    res.setHeader("Content-Type", "image/png");
+    res.status(200).send(image);
+
+  } catch (err) {
+    res.status(500).send("Error generating wallpaper");
+  }
 }
